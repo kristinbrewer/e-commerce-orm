f911928d5c1bb3f7f00815e8e2500a25061754b6
Kristin Brewer <kr527106@gmail.com>
2023-01-31 09:27:03 -0500

    columns for models entered: category, prodect, productTag, tag JS

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,41 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        //validates value is numeric
+        isNumeric: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //default value
+      defaultValue: 10,
+      validate: {
+        //validates value is decimal
+        isDecimal: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }  
+    }
   },
   {
     sequelize,
